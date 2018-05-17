aae8096f5046dcb198f60a6543a0a0c8e7f2a863
Jarosław Wieczorek <jaroslaw.wieczorek@sealcode.org>
2018-05-17 16:42:14 +0200

    small fixes

--- env/EvilPostman/dokumentacja/_build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,indek:0,modułów:0,wyszukiwani:0},titles:["Welcome to EvilPostman&#8217;s documentation!"],titleterms:{document:0,evilpostman:0,indic:0,tabl:0,welcom:0}})
